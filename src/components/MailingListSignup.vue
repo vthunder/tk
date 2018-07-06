adedd99d6a9a2a96673b631ca831f2b03dd63751
Dan Mills <hello@sandmill.org>
2018-07-05 23:33:07 -0700

    add mailing list signup widget

---
+++ src/components/MailingListSignup.vue
@@ -0,0 +1,98 @@
+<template>
+    <div>
+        <b-alert :show=success variant="success">
+            {{ successMsg }}
+        </b-alert>
+        <b-alert :show=failure variant="danger">
+            {{ failureMsg }}
+        </b-alert>
+        <b-form @submit="onSubmit" @reset="onReset" v-if="showForm">
+            <div class="cta-text">{{ ctaText }}</div>
+            <b-form-group label="Email" label-sr-only label-for="email-field">
+                <b-input-group>
+                    <b-form-input id="email-field"
+                                  type="email"
+                                  v-model="form.email"
+                                  required
+                                  placeholder="your.email@example.com">
+                    </b-form-input>
+                    <b-input-group-append>
+                        <b-button type="submit" :variant=btnVariant>Subscribe</b-button>
+                    </b-input-group-append>
+                </b-input-group>
+            </b-form-group>
+        </b-form>
+    </div>
+</template>
+
+<script>
+import * as auth from '../graphql/auth';
+import * as misc from '../graphql/misc';
+
+export default {
+  props: ['cta', 'variant'],
+  data() {
+    return {
+      btnVariant: this.variant ? this.variant : 'primary',
+      ctaText: this.cta ? this.cta :
+        'Sign up below to hear the latest and get on our membership waitlist.',
+      me: null,
+      success: false,
+      successMsg: 'Thanks! Check your email to confirm your subscription.',
+      failure: false,
+      failureMsg: 'Oops, something went wrong. Maybe you mistyped your email?',
+      form: {
+        email: '',
+      },
+      showForm: true,
+    };
+  },
+  apollo: {
+    me: auth.query.me,
+  },
+  methods: {
+    onSubmit() {
+      this.success = false;
+      this.failure = false;
+
+      const mutation = misc.mutation.mailingListSignup;
+      const variables = {
+        email: this.form.email,
+      };
+
+      this.$apollo.mutate({
+        mutation,
+        variables,
+      }).then(() => {
+        this.success = true;
+        this.showForm = false;
+      }).catch(({ graphQLErrors: [{ message }] }) => {
+        switch (message) {
+          case 'Member Exists':
+            this.successMsg = 'Success! But... it seems you were already subscribed.';
+            this.success = true;
+            this.showForm = false;
+            break;
+          default:
+            this.failure = true;
+        }
+      });
+    },
+    onReset() {
+      this.success = false;
+      this.failure = false;
+      this.form.email = '';
+      /* Trick to reset/clear native browser form validation state */
+      this.showForm = false;
+      this.$nextTick(() => { this.showForm = true; });
+    },
+  },
+};
+</script>
+
+<style>
+.cta-text {
+    font-family: museo;
+    font-weight: 300;
+}
+</style>
