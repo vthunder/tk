ac0d1c18318da7a0b9d0439ed89653f679e840c1
Dan Mills <hello@sandmill.org>
2018-07-05 23:33:52 -0700

    add footer

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,20 @@
         <router-view/>
         <AuthModal/>
         <WelcomeModal/>
-        <div id="nav">
-            <router-link to="/">Home</router-link> |
-            <router-link to="/about">About</router-link>
-        </div>
+        <Footer/>
     </div>
 </template>
 
 <script>
 import Nav from '@/components/Nav.vue';
+import Footer from '@/components/Footer.vue';
 import AuthModal from '@/components/AuthModal.vue';
 import WelcomeModal from './components/WelcomeModal.vue'; // @ not working here?
 
 export default {
   components: {
     Nav,
+    Footer,
     AuthModal,
     WelcomeModal,
   },
---
+++ src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="footer-section bg-dark text-white">
+        <div class="container section">
+            <div class="row justify-content-center">
+                <div class="col-md-10">
+                    <MailingListSignup />
+                    <div class="footer-social-links">
+                        <span class="follow-us-text">Follow us on</span>
+                        <a href="https://instagram.com/tinkerkitch/" class="social-icon"><img src="/images/Instagram-White2x.png" width="24"></a>
+                        <a href="https://twitter.com/tinkerkitch" class="social-icon"><img src="/images/Twitter-White2x.png" width="24"></a>
+                        <a href="https://www.facebook.com/tinkerkitchen" class="social-icon"><img src="/images/Facebook-White2x.png" width="24"></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="copyright">© 2018 Tinker Kitchen Inc. All rights reserved.</div>
+        </div>
+    </div>
+</template>
+
+<script>
+import MailingListSignup from '@/components/MailingListSignup.vue';
+
+export default {
+  components: {
+    MailingListSignup,
+  },
+};
+</script>
