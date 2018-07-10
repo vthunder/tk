18d407f01d19724226df2f1f0cc743d9aad213d6
Dan Mills <hello@sandmill.org>
2018-07-09 20:52:22 -0700

    add RequireSignIn component that shows content only when user is signed in, or a sign in button otherwise

---
+++ src/components/RequireSignIn.vue
@@ -0,0 +1,58 @@
+<template>
+    <div v-if="me">
+        <slot></slot>
+    </div>
+    <div v-else class="need-auth">
+        <span>
+            {{ prop.pre_text }}
+            <b-button class="mx-1" :variant=prop.variant @click=showModal>
+                Sign in <i class="fas fa-sign-in-alt"></i>
+            </b-button>
+            {{ prop.post_text }}
+        </span>
+    </div>
+</template>
+
+<script>
+import * as auth from '../graphql/auth';
+
+export default {
+  props: ['pre_text', 'post_text', 'variant', 'next_route'],
+  data() {
+    return {
+      me: '',
+      prop: {
+        pre_text: (this.pre_text || ''),
+        post_text: (this.post_text || 'to access this page.'),
+        variant: (this.variant || 'primary'),
+        next_route: (this.next_route || ''),
+      },
+    };
+  },
+  apollo: {
+    me: auth.query.me,
+  },
+  methods: {
+    showModal() {
+      if (this.prop.next_route) {
+        localStorage.nextRoute = JSON.stringify(this.prop.next_route);
+      }
+      this.$root.$emit('bv::show::modal', 'auth-modal');
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.need-auth {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100%;
+    width: 100%;
+
+    span {
+        text-align: center;
+    }
+}
+</style>
