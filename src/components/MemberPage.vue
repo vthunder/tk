c5dbdf9c3367af2f1fe9ef0794598185530dd4b4
Dan Mills <hello@sandmill.org>
2018-07-23 16:37:57 -0700

    use named routes instead of paths

--- src/components/MemberPage.vue
+++ src/components/MemberPage.vue
@@ -5,12 +5,12 @@
                 <b-row>
                     <b-col cols="4" md="3">
                         <b-nav class="member-nav" vertical pills variant="secondary">
-                            <b-nav-item to="/member/membership">Membership</b-nav-item>
-                            <b-nav-item to="/member/daypasses'">Day Passes</b-nav-item>
-                            <b-nav-item to="/member/billing">Billing</b-nav-item>
+                            <b-nav-item :to="{ name: 'member-membership' }">Membership</b-nav-item>
+                            <b-nav-item :to="{ name: 'member-daypasses' }">Day Passes</b-nav-item>
+                            <b-nav-item :to="{ name: 'member-billing' }">Billing</b-nav-item>
                         </b-nav>
                     </b-col>
-                    <b-col cols="8" md="9">
+                    <b-col class="member-page-content" cols="8" md="9">
                         <slot></slot>
                     </b-col>
                 </b-row>
@@ -31,6 +31,9 @@ export default {
 
 <style lang="scss">
 .member-nav.nav-pills {
+    border-right: 1px solid lightgray;
+    height: 100%;
+
     .nav-link {
         color: gray;
     }
@@ -38,5 +41,8 @@ export default {
         color: white;
         background-color: gray;
     }
+}
+.member-page-content {
+    padding: 20px;
 }
 </style>
