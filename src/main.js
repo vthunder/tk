b4101606b4f1b068881535c7dfbdbe10dd66ff3b
Dan Mills <hello@sandmill.org>
2018-07-29 15:46:37 -0700

    disable vue-analytics debug

--- src/main.js
+++ src/main.js
@@ -22,7 +22,6 @@ Vue.use(VueStripeCheckout, {
 Vue.use(VueAnalytics, {
   id: 'UA-54365170-1',
   debug: {
-    enabled: true,
     sendHitTask: process.env.VUE_APP_ENV === 'production',
   },
   router
