c2f6a96b433fd07e169f75757e84ff25a89cfcdf
Dan Mills <hello@sandmill.org>
2018-08-24 16:55:43 -0700

    secondary btn variant for footer mailing list signup

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
         <div class="container section">
             <div class="row justify-content-center">
                 <div class="col-md-10">
-                    <MailingListSignup />
+                    <MailingListSignup variant="secondary" />
                     <div class="footer-social-links">
                         <span class="follow-us-text">Follow us on</span>
                         <a href="https://instagram.com/tinkerkitch/" class="social-icon"><img src="/images/Instagram-White2x.png" width="24"></a>
