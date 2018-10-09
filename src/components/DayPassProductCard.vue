381a817f87bd38e35adeda3e8b367673a03af844
Dan Mills <hello@sandmill.org>
2018-10-09 10:58:33 +0800

    product card to buy a day pass sku

---
+++ src/components/DayPassProductCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <b-card :key=sku.id
+            :title=sku.attributes.title
+            :sub-title="sku.attributes.subtitle">
+        <p class="card-text">{{ sku.attributes.description }}</p>
+        <div slot="footer" class="text-center">
+            <b-button variant="primary" @click="buy(sku)">
+                Buy
+            </b-button>
+        </div>
+    </b-card>
+</template>
+
+<script>
+export default {
+  props: ['sku'],
+};
+</script>
+
+<style lang="scss">
+</style>
