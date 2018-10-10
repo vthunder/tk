dc7837ce12d7d0f9a2517dd6a5e4ee8b92cdd0fe
Dan Mills <hello@sandmill.org>
2018-10-10 16:58:57 +0800

    flesh out group events page content

--- src/views/Groups.vue
+++ src/views/Groups.vue
@@ -1,25 +1,60 @@
 <template>
     <b-container>
+        <h1 class="mt-4">Group Events</h1>
+
+        <p>There are several ways to have a group activity at Tinker
+            Kitchen! We're a perfect space for a small cooking-themed
+            team offsite, dinner party, or even a larger celebration.</p>
+
         <b-row class="justify-content-center">
-            <b-col md="5" class="feature-table">
-                <h4>Groups</h4>
-                <ul class="list-unstyled">
-                    <li>Hold small team off-sites with a
-                        cooking-themed activity</li>
-                    <li>Book the whole space all to yourself for
-                        your special private events: parties,
-                        product launches, or private dinners</li>
-                    <li>Have an idea for a special pop-up event?
-                        Partner with us for a class, demonstration,
-                        or talk</li>
-                </ul>
+            <b-col md="4" lg="3">
+                <b-card title="Team-building events" class="mt-2">
+                    <p>Flat $750,<br>Up to 15 people</p>
+                    <p>We can help you put together a fun
+                        cooking-themed team offsite!</p>
+                    <p>Talk to us about timing and options
+                        (chocolate-making, ice cream, etc). We're
+                        happy to customize to your needs.</p>
+                    <p>Note that Tinker Kitchen remains open for
+                        members, but we do reserve space for the
+                        activity.</p>
+                </b-card>
+            </b-col>
+            <b-col md="4" lg="3">
+                <b-card title="Full space rental" class="mt-2">
+                    <p>Flat $1500-$2000,<br>Up to 40 people</p>
+                    <p>Exclusive use for your private party, product
+                        launch, or other activity/celebration.</p>
+                    <p>Tinker Kitchen staff can provide guidance, help
+                        with logistics, etc, and you can hire
+                        additional crew depending on the
+                        activity. Note: 1 month notice required to
+                        inform our members that we’ll be closed on
+                        that day.</p>
+                </b-card>
+            </b-col>
+            <b-col md="4" lg="3">
+                <b-card title="DIY dinner party" class="mt-2">
+                    <p>$25-35/person<br>Up to 25 people</p>
+                    <p>For a more informal/DIY approach, we also offer
+                        day passes. No guided activity included, but
+                        we can help you with logistics, receiving
+                        ingredients, etc.</p>
+                    <p>Use of the space is first-come, first-serve (no
+                        reservations), but we can give you advice on
+                        best timing and recommended maximum (up to 40
+                        people).</p>
+                </b-card>
             </b-col>
         </b-row>
+        <p class="mt-4 text-center">
+            <b-button href="mailto:hello@tinkerkitchen.org" variant="primary">
+                Contact us
+            </b-button><br>
+        </p>
+        <p class="text-center">email: hello@tinkerkitchen.org</p>
     </b-container>
 </template>
-
-<script>
-</script>
 
 <style lang="scss">
 </style>
