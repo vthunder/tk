26fa03078aa9b62879214e4411845b290fba4682
Dan Mills <hello@sandmill.org>
2018-10-10 17:14:05 +0800

    flesh out commercial use page content

--- src/views/Commercial.vue
+++ src/views/Commercial.vue
@@ -1,27 +1,39 @@
 <template>
     <b-container>
+        <h1 class="mt-4">Commercial Use</h1>
+
         <b-row class="justify-content-center">
-            <b-col md="5" class="feature-table">
-                <h4>Commercial use</h4>
-                <ul class="list-unstyled">
-                    <li>Practice your skills&#8212;We're a perfect
-                        complement to a culinary degree program</li>
-                    <li>Create new recipes and test product
-                        ideas. We're the perfect venue for all your
-                        R&D, and have a network of mentors and
-                        experts you can access</li>
-                    <li>Host micro-popup events and teach classes to
-                        get feedback and refine your products</li>
-                    <li class="small">Note: no food production for
-                        sale allowed at this time</li>
-                </ul>
+            <b-col md="10">
+                <p>Although we don't allow food production at this time, we
+                    <strong>are</strong> a commercial kitchen
+                    facility. Whether you recently graduated from a
+                    culinary degree program and are looking for a
+                    place to practice your skills and try our new
+                    ideas, or a serial entrepreneur looking to do
+                    focused R&D and get a new product to market,
+                    Tinker Kitchen is here for you.</p>
+
+                <p>We have a network of mentors and experts that can help you
+                    get to market faster. Whether you need help with
+                    regulatory compliance, sourcing a good vendor for an
+                    ingredient, designing packaging, or making sure your green
+                    drink stays drink on the shelf (we hear that is
+                    difficult!)&mdash;we can help connect you to the right
+                    expert.</p>
+
+                <p>Because we're a great events space, we can also
+                    host micro-popup events and classes/demos designed
+                    to get customer feedback and refine your products.</p>
             </b-col>
         </b-row>
+        <p class="mt-2 text-center">
+            <b-button href="mailto:hello@tinkerkitchen.org" variant="primary">
+                Contact us
+            </b-button><br>
+        </p>
+        <p class="text-center">email: hello@tinkerkitchen.org</p>
     </b-container>
 </template>
-
-<script>
-</script>
 
 <style lang="scss">
 </style>
