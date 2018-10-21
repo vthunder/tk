e14a38c12cda067eff3a0b3373810ec81830bab4
Dan Mills <hello@sandmill.org>
2018-10-20 18:05:14 -0700

    update group events copy

--- src/views/Groups.vue
+++ src/views/Groups.vue
@@ -7,7 +7,8 @@
             team offsite, dinner party, or even a larger celebration.</p>
 
         <b-row class="justify-content-center">
-            <b-col md="4" lg="3">
+            <!--
+            <b-col md="4" lg="4">
                 <b-card title="Team-building events" class="mt-2">
                     <p>Flat $750,<br>Up to 15 people</p>
                     <p>We can help you put together a fun
@@ -20,30 +21,44 @@
                         activity.</p>
                 </b-card>
             </b-col>
-            <b-col md="4" lg="3">
+            -->
+            <b-col md="4" lg="4">
+                <b-card title="Partial rental" class="mt-2">
+                    <p>$75/hr + $50 cleaning<br>Up to 12 people</p>
+                    <p>Get access to our space for your team-building
+                        activity, pop-up demo, class, or other
+                        activity / gathering. The hourly rate covers
+                        all your guests (~12 max at a time), no need
+                        for other day passes/membership.</p>
+                    <p>We'll reserve a large 2-table cooking workspace
+                        plus seating space as needed, good for up to
+                        ~12 people.</p>
+                </b-card>
+            </b-col>
+            <b-col md="4" lg="4">
+                <b-card title="Member dinner party" class="mt-2">
+                    <p>$50/hr + $50 cleaning (waivable)<br>Up to 12 people</p>
+                    <p>We love to host dinner parties for our members!
+                        So we have a special low rental rate for
+                        them. Your guests don't need to get day
+                        passes. And we'll even waive the $50 cleaning
+                        fee if everything is left clean!</p>
+                    <p>We'll reserve a large 2-table cooking workspace
+                        plus seating space as needed, good for up to
+                        ~12 people.</p>
+                </b-card>
+            </b-col>
+            <b-col md="4" lg="4">
                 <b-card title="Full space rental" class="mt-2">
                     <p>Flat $1500-$2000,<br>Up to 40 people</p>
                     <p>Exclusive use for your private party, product
                         launch, or other activity/celebration.</p>
-                    <p>Tinker Kitchen staff can provide guidance, help
-                        with logistics, etc, and you can hire
-                        additional crew depending on the
-                        activity. Note: 1 month notice required to
-                        inform our members that we’ll be closed on
-                        that day.</p>
-                </b-card>
-            </b-col>
-            <b-col md="4" lg="3">
-                <b-card title="DIY dinner party" class="mt-2">
-                    <p>$25-35/person<br>Up to 25 people</p>
-                    <p>For a more informal/DIY approach, we also offer
-                        day passes. No guided activity included, but
-                        we can help you with logistics, receiving
-                        ingredients, etc.</p>
-                    <p>Use of the space is first-come, first-serve (no
-                        reservations), but we can give you advice on
-                        best timing and recommended maximum (up to 40
-                        people).</p>
+                    <p>We'll reserve the entire Tinker Kitchen space
+                        for you for 1/2 day (6h) or a whole day, and
+                        we'll also help you with guidance, logistics,
+                        and hiring staff if needed. One month notice
+                        required to inform our members that we’ll be
+                        closed on that day.</p>
                 </b-card>
             </b-col>
         </b-row>
