9daa8431fc89a6f35af5e2de09d802a7a465eb20
Dan Mills <hello@sandmill.org>
2018-10-22 23:38:56 -0700

    add account and redeem coupon pages to left nav

--- src/components/MemberPage.vue
+++ src/components/MemberPage.vue
@@ -5,10 +5,12 @@
                 <b-row>
                     <b-col cols="4" md="3">
                         <b-nav class="member-nav" vertical pills variant="secondary">
+                            <b-nav-item :to="{ name: 'member-account' }">Account</b-nav-item>
                             <b-nav-item :to="{ name: 'member-membership' }">Membership</b-nav-item>
                             <b-nav-item :to="{ name: 'member-daypasses' }">Day Passes</b-nav-item>
                             <b-nav-item :to="{ name: 'member-rentals' }">Rentals</b-nav-item>
                             <b-nav-item :to="{ name: 'member-billing' }">Billing</b-nav-item>
+                            <b-nav-item :to="{ name: 'member-coupon' }">Redeem Coupon</b-nav-item>
                         </b-nav>
                     </b-col>
                     <b-col class="member-page-content" cols="8" md="9">
