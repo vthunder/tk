9e259c983585771cf2f93a84fcdb60321ca774bb
Dan Mills <hello@sandmill.org>
2018-10-23 15:40:30 -0700

    hook up 'go to account' button in welcome modal

--- src/components/WelcomeModal.vue
+++ src/components/WelcomeModal.vue
@@ -1,7 +1,18 @@
 <template>
     <b-modal id="welcome-modal" ref="welcomeModal" title="Welcome!"
-             ok-title="Go to account" ok-only centered>
+             ok-title="Go to account" ok-only centered
+             @ok="goToAccount">
         <p>Welcome to Tinker Kitchen! Hope to cook up a storm with you
             at our makerspace :-)</p>
     </b-modal>
 </template>
+
+<script>
+export default {
+  methods: {
+    goToAccount() {
+      this.$router.push({ name: 'member-membership' });
+    },
+  },
+};
+</script>
