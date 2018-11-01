cf358542e996d8d560787d8a0567c1bbfbcbc7a9
Dan Mills <hello@sandmill.org>
2018-11-01 16:18:04 -0700

    tweak group options: add team building activity pricing, add larger partial rental option

--- src/views/Groups.vue
+++ src/views/Groups.vue
@@ -7,57 +7,53 @@
             team offsite, dinner party, or even a larger celebration.</p>
 
         <b-row class="justify-content-center">
-            <!--
-            <b-col md="4" lg="4">
-                <b-card title="Team-building events" class="mt-2">
-                    <p>Flat $750,<br>Up to 15 people</p>
-                    <p>We can help you put together a fun
-                        cooking-themed team offsite!</p>
-                    <p>Talk to us about timing and options
-                        (chocolate-making, ice cream, etc). We're
-                        happy to customize to your needs.</p>
-                    <p>Note that Tinker Kitchen remains open for
-                        members, but we do reserve space for the
-                        activity.</p>
-                </b-card>
-            </b-col>
-            -->
-            <b-col md="4" lg="4">
-                <b-card title="Partial rental" class="mt-2">
-                    <p>$75/hr + $50 cleaning<br>Up to 12 people</p>
+            <b-col sm="6" lg="5">
+                <b-card title="Partial rental"
+                        sub-title="$75-140/hr + cleaning" class="mt-2">
                     <p>Get access to our space for your team-building
                         activity, pop-up demo, class, or other
-                        activity / gathering. The hourly rate covers
-                        all your guests (~12 max at a time), no need
-                        for other day passes/membership.</p>
-                    <p>We'll reserve a large 2-table cooking workspace
-                        plus seating space as needed, good for up to
-                        ~12 people.</p>
+                        activity / gathering.</p>
+                    <p>We'll reserve cooking workspaces and seating
+                        space as needed, for groups of up to 12 (2
+                        tables) or 24 (4 tables).</p>
                 </b-card>
             </b-col>
-            <b-col md="4" lg="4">
-                <b-card title="Member dinner party" class="mt-2">
-                    <p>$50/hr + $50 cleaning (waivable)<br>Up to 12 people</p>
-                    <p>We love to host dinner parties for our members!
-                        So we have a special low rental rate for
-                        them. Your guests don't need to get day
-                        passes. And we'll even waive the $50 cleaning
-                        fee if everything is left clean!</p>
-                    <p>We'll reserve a large 2-table cooking workspace
-                        plus seating space as needed, good for up to
-                        ~12 people.</p>
-                </b-card>
-            </b-col>
-            <b-col md="4" lg="4">
-                <b-card title="Full space rental" class="mt-2">
-                    <p>$200/hr + $100 cleaning<br>Up to 40 people</p>
+            <b-col sm="6" lg="5">
+                <b-card title="Full space rental"
+                        sub-title="$200/hr + $150 cleaning" class="mt-2">
                     <p>Exclusive use for your private party, product
                         launch, or other activity/celebration.</p>
-                    <p>4 hour minimum. We'll reserve the entire Tinker
-                        Kitchen space for you , and we'll also help
-                        you with guidance, logistics, and hiring staff
-                        if needed. One month notice required to inform
-                        our members that we’ll be closed on that day.</p>
+                    <p>We'll reserve the entire Tinker Kitchen space
+                        for you (up to 40 guests), and help you with
+                        guidance, logistics, and hiring staff if
+                        needed. 2 weeks advance booking required.</p>
+                </b-card>
+            </b-col>
+        </b-row>
+        <b-row class="justify-content-center">
+            <b-col sm="6" lg="5">
+                <b-card title="Team-building activity"
+                        sub-title="$40-100/person + rental" class="mt-2">
+                    <p>Looking for a guided activity? Add an activity
+                        to a rental! We can help you put together a
+                        fun cooking-themed team offsite for your team.</p>
+                    <p>Whether you're looking for a short afternoon
+                        activity, or a guided multi-course dinner
+                        party--we can make it happen at Tinker Kitchen
+                        :)</p>
+                </b-card>
+            </b-col>
+            <b-col sm="6" lg="5">
+                <b-card title="Member dinner party"
+                        sub-title="$50/hr + cleaning" class="mt-2">
+                    <p>We love to host dinner parties for our members!
+                        If you're a Tinker Kitchen member, we have a
+                        special low rental rate for you: just $50/hr,
+                        your guests don't need to get day passes. And
+                        we'll even waive the cleaning fee if
+                        everything is left clean!</p>
+                    <p>We'll reserve cooking workspace and seating
+                        space as needed for up to 8 people.</p>
                 </b-card>
             </b-col>
         </b-row>
