edb6e5510d9352508bb1df08400004341f526f69
Dan Mills <hello@sandmill.org>
2018-11-14 01:25:14 -0800

    fix too long line

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -34,7 +34,9 @@
                     <div v-if="calendar_event.ext_book_url">
                         <b-button :href="calendar_event.ext_book_url"
                                   variant="primary">Book Event</b-button>
-                        <div v-if="me && me.is_member">Member discount code: {{ calendar_event.ext_member_discount_code }}</div>
+                        <div v-if="me && me.is_member">
+                            Member discount code: {{ calendar_event.ext_member_discount_code }}
+                        </div>
                     </div>
                     <div v-else>
                         <div v-if="me">
