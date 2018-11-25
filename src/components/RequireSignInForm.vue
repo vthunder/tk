c29df117c4f9f45fcee796d9d0d1db4066640794
Dan Mills <hello@sandmill.org>
2018-11-24 17:23:10 -0800

    RequireSignInForm: remove old code carried over from RequreSignIn component; pass along next_action to AuthForm

--- src/components/RequireSignInForm.vue
+++ src/components/RequireSignInForm.vue
@@ -3,7 +3,7 @@
         <slot></slot>
     </div>
     <div v-else>
-        <AuthForm/>
+        <AuthForm :next_route="next_route" :next_action="next_action" />
     </div>
 </template>
 
@@ -12,22 +12,10 @@ import * as auth from '@/graphql/auth';
 import AuthForm from '@/components/AuthForm.vue';
 
 export default {
-  props: ['pre_text', 'btn_text', 'btn_icon', 'post_text',
-    'auth_class', 'variant', 'next_route', 'next_action'],
+  props: ['next_route', 'next_action'],
   data() {
     return {
       me: '',
-      enable_login: process.env.VUE_APP_ENABLE_LOGIN === 'true',
-      prop: {
-        pre_text: (this.pre_text || ''),
-        btn_text: (this.btn_text || 'Sign in'),
-        btn_icon: (this.btn_icon || 'fa-sign-in-alt'),
-        post_text: (this.post_text || 'to access this page.'),
-        auth_class: (this.auth_class || 'center'),
-        variant: (this.variant || 'primary'),
-        next_route: (this.next_route || ''),
-        next_action: (this.next_action || ''),
-      },
     };
   },
   apollo: {
@@ -37,17 +25,6 @@ export default {
         if (data.me) return data.me;
         return null;
       },
-    },
-  },
-  methods: {
-    showModal() {
-      if (this.prop.next_route) {
-        localStorage.nextRoute = JSON.stringify(this.prop.next_route);
-      }
-      if (this.prop.next_action) {
-        localStorage.nextAction = this.prop.next_action;
-      }
-      this.$root.$emit('bv::show::modal', 'auth-modal');
     },
   },
   components: {
