157a68f36b91cf75606d985e5b20e308c4431abf
Dan Mills <hello@sandmill.org>
2018-12-08 21:29:05 -0800

    tweak a couple of answers; make questions pop a little more in style

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -1,9 +1,9 @@
 <template>
-    <b-container>
-        <h1 class="mt-4">FAQ</h1>
-
-        <b-row class="justify-content-center">
+    <b-container class="faq">
+        <b-row class="justify-content-center mt-4">
             <b-col sm="10" lg="8">
+                <h1>FAQ</h1>
+
                 <h3>What is Tinker Kitchen?</h3>
                 <p>Tinker Kitchen is a makerspace for food and
                     cooking.</p>
@@ -14,49 +14,41 @@
                     member. If you pay monthly, you can come anytime
                     and bring a +1 with you as well, plus discounts on
                     classes and other goodies.</p>
-            </b-col>
-            <b-col sm="10" lg="8">
+
                 <h3>Can my friend come just to eat?</h3>
                 <p>If you're a member, you can bring a +1
-                    anytime. Additional people need to get a day
-                    pass. There's no "eating only" pass.</p>
-                <p>We do have <b-link :to="{ name: 'groups' }">group pricing options</b-link>,
-                    though (e.g. for a dinner party).</p>
-            </b-col>
-            <b-col sm="10" lg="8">
+                    anytime. Additional people need to get a day pass.</p>
+                <p>Members can also purchase "eating only" passes for
+                extra friends coming to a dinner party, more details
+                on our <b-link :to="{ name: 'groups' }">group
+                pricing page</b-link>.</p>
+
                 <h3>What are your opening hours?</h3>
                 <p>We're open daily from 10AM - 9PM, except on
                     Tuesdays.</p>
-            </b-col>
-            <b-col sm="10" lg="8">
+
                 <h3>Is there an age requirement?</h3>
                 <p>Tinker Kitchen is open to all ages. Children under
                     3 years of age enter free of charge. All minors
                     must be accompanied by an adult.</p>
                 <p>Note: our classes may have separate age
                     requirements, often 16 and up.</p>
-            </b-col>
-            <b-col sm="10" lg="8">
+
                 <h3>Is there bicycle parking?</h3>
                 <p>Yes! There's bicycle parking right across the
                     street.</p>
-            </b-col>
-            <b-col sm="10" lg="8">
+
                 <h3>What's the closest BART stop?</h3>
                 <p>We're just 2 blocks away from the 24th St BART
                     station.</p>
-            </b-col>
-            <b-col sm="10" lg="8">
+
                 <h3>I'm driving there. Where can I park?</h3>
-                <p>We're close to public transit, but if you're
-                    driving we do have parking nearby.</p>
                 <p>There's metered street parking in front (except on
                     Thursday afternoons when the farmer's market is
-                    there).</p>
+                    there). You can also drive around the side streets
+                    to look for non-metered parking (2-hour limit).</p>
                 <p>There's also a garage half a block away, look for
-                    the "Mission Bartlett Garage". You can also drive
-                    around the side streets to look for non-metered
-                    parking (2-hour limit, at least in theory).</p>
+                    the "Mission Bartlett Garage".</p>
             </b-col>
         </b-row>
 
@@ -72,4 +64,7 @@ export default {
 </script>
 
 <style lang="scss">
+.faq {
+    p { padding-left: 1em; }
+}
 </style>
