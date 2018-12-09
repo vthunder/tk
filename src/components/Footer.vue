6efc4f14e9cb904946a74e53c861bf0d1398f079
Dan Mills <hello@sandmill.org>
2018-12-08 21:24:36 -0800

    new footer with links and better mailing list signup styling

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,20 +1,43 @@
 <template>
-    <div class="footer-section bg-dark text-white">
-        <div class="container section">
-            <div class="row justify-content-center">
-                <div class="col-md-10">
-                    <MailingListSignup variant="secondary" />
-                    <div class="footer-social-links">
-                        <span class="follow-us-text">Follow us on</span>
-                        <a href="https://instagram.com/tinkerkitch/" class="social-icon"><img src="/images/Instagram-White2x.png" width="24"></a>
-                        <a href="https://twitter.com/tinkerkitch" class="social-icon"><img src="/images/Twitter-White2x.png" width="24"></a>
-                        <a href="https://www.facebook.com/tinkerkitchen" class="social-icon"><img src="/images/Facebook-White2x.png" width="24"></a>
-                    </div>
-                </div>
-            </div>
-
+    <div>
+        <b-container fluid class="ml-signup mt-4 py-4">
+            <b-row class="justify-content-center">
+                <b-col md="10">
+                    <MailingListSignup class="footer-ml-signup" variant="secondary" />
+                </b-col>
+            </b-row>
+        </b-container>
+        <b-container fluid class="bg-dark text-white">
+            <b-row class="footer-links justify-content-center pt-4">
+                <b-col md="3" offset-md="1">
+                    <ul>
+                        <li><h5>Cook</h5></li>
+                        <li><b-link :to="{ name: 'classes' }">Classes</b-link></li>
+                        <li><b-link :to="{ name: 'learn-daypasses' }">Day passes</b-link></li>
+                        <li><b-link :to="{ name: 'learn-membership' }">Membership</b-link></li>
+                        <li><b-link :to="{ name: 'groups' }">Groups & Events</b-link></li>
+                    </ul>
+                </b-col>
+                <b-col md="3">
+                    <ul>
+                        <li><h5>About</h5></li>
+                        <li><b-link :to="{ name: 'about' }">Location & Hours</b-link></li>
+                        <li><b-link :to="{ name: 'about' }">Equipment</b-link></li>
+                        <li><b-link :to="{ name: 'calendar' }">Calendar</b-link></li>
+                        <li><b-link :to="{ name: 'faq' }">FAQ</b-link></li>
+                    </ul>
+                </b-col>
+                <b-col md="3">
+                    <ul>
+                        <li><h5>Follow</h5></li>
+                        <li><b-link href="https://instagram.com/tinkerkitch/">Instagram</b-link></li>
+                        <li><b-link href="https://twitter.com/tinkerkitch">Twitter</b-link></li>
+                        <li><b-link href="https://www.facebook.com/tinkerkitchen">Facebook</b-link></li>
+                    </ul>
+                </b-col>
+            </b-row>
             <div class="copyright">© 2018 Tinker Kitchen Inc. All rights reserved.</div>
-        </div>
+        </b-container>
     </div>
 </template>
 
@@ -29,47 +52,42 @@ export default {
 </script>
 
 <style lang="scss">
-.footer-section {
-    padding-top: 15px;
-}
-.footer-section .section {
-    margin-top: 0;
-    margin-bottom: 0;
+.ml-signup {
+    background-color: #ccc;
 }
 
-.footer-ml-signup input,
-.footer-ml-signup button {
-    border: none;
-    height: 54px;
-    margin-top: 10px;
+.footer-ml-signup {
+    input, button {
+        border: none;
+        height: 54px;
+        margin-top: 10px;
+    }
+    button {
+        background-color: #ff7911;
+        padding: 6px 36px;
+        transition: background-color 250ms;
+
+        &:hover { background-color: #cc4600; }
+    }
 }
-.footer-ml-signup button {
-    background-color: #ff7911;
-    box-shadow: 0 3px 0 0 #aa3400;
-    padding: 6px 36px;
-    transition: background-color 250ms;
+
+.footer-links {
+    ul {
+        list-style: none;
+    }
+    a {
+        color: white;
+        opacity: 0.8;
+        transition: opacity 250ms;
+
+        &:hover { opacity: 1; }
+    }
 }
-.footer-ml-signup button:hover {
-    background-color: #cc4600;
-}
-.footer-social-links {
-    padding: 20px 0;
-    text-align: center;
-}
-.footer-social-links a {
-    padding-left: 10px;
-}
+
 .copyright {
     color: hsla(0, 0%, 100%, .29);
     font-weight: 300;
-    padding-top: 30px;
     text-align: center;
-}
-.social-icon {
-    opacity: 0.8;
-    transition: opacity 250ms;
-}
-.social-icon:hover {
-    opacity: 1;
+    margin-top: 2em;
 }
 </style>
