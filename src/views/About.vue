beb3220d94086526425aad299ba6a397d6593cde
Dan Mills <hello@sandmill.org>
2018-12-17 13:21:31 -0800

    rearrange and add ids to about page sections

--- src/views/About.vue
+++ src/views/About.vue
@@ -23,31 +23,7 @@
                         temperers</p>
                 </div>
 
-                <h2 class="text-center mt-4">Our Story</h2>
-
-                <p>Hi, I’m Dan Mills, founder of Tinker Kitchen.</p>
-                <p>Food and cooking have always been a hobby for me, and I’ve always had a
-                    love for kitchen equipment and the science of food. From my childhood
-                    in Venezuela through my career in the tech industry I’ve slowly expanded
-                    my arsenal of tastes, techniques and equipment, always tinkering with
-                    something new. In 2015 I started Tinker Kitchen to build an awesome maker
-                    space for food where I can share this passion with others.</p>
-                <p>The seed for this concept dates back to my college days, where I had the
-                    good fortune to take a very special biology course. Makerspaces weren’t
-                    really a thing back then, but that’s exactly what this was: a sculptor
-                    and a bio prof had a lab in the basement to explain real-world structures
-                    and processes by building physical models. We studied biological
-                    organisms, did research, and built models to explain them. I discovered
-                    that this learning style is not only great fun, but immensely effective:
-                    I can still remember the things I learned back then, even though it’s been
-                    almost two decades.<a href="#" class="link"></a></p>
-                <p>That is how I approach cooking as well. I am not only interested in the end
-                    product, but in learning the underlying principles. And there is no better
-                    way to learn than by having hands-on fun in a highly creative makerspace,
-                    with all the tools you could ever dream of, and a community to lend a
-                    hand.<a href="#" class="link"></a></p>
-
-                <h2 class="text-center mt-4">Location & Hours</h2>
+                <h2 id="location-hours" class="text-center mt-4">Location & Hours</h2>
 
                 <p>We're in the Mission District in San Francisco.</p>
 
@@ -60,7 +36,7 @@
                         :src=maps.src
                         allowfullscreen></iframe>
 
-                <h2 class="text-center my-4">Equipment List</h2>
+                <h2 id="equipment" class="text-center my-4">Equipment List</h2>
 
                 <p>We're a little obsessed with kitchen
                     equipment. Tinker Kitchen is a bit like a "kitchen
@@ -92,6 +68,30 @@
                     the highlights!</p>
 
                 <b-table :items="items" :fields="fields" striped outlined></b-table>
+
+                <h2 id="our-story" class="text-center mt-4">Our Story</h2>
+
+                <p>Hi, I’m Dan Mills, founder of Tinker Kitchen.</p>
+                <p>Food and cooking have always been a hobby for me, and I’ve always had a
+                    love for kitchen equipment and the science of food. From my childhood
+                    in Venezuela through my career in the tech industry I’ve slowly expanded
+                    my arsenal of tastes, techniques and equipment, always tinkering with
+                    something new. In 2015 I started Tinker Kitchen to build an awesome maker
+                    space for food where I can share this passion with others.</p>
+                <p>The seed for this concept dates back to my college days, where I had the
+                    good fortune to take a very special biology course. Makerspaces weren’t
+                    really a thing back then, but that’s exactly what this was: a sculptor
+                    and a bio prof had a lab in the basement to explain real-world structures
+                    and processes by building physical models. We studied biological
+                    organisms, did research, and built models to explain them. I discovered
+                    that this learning style is not only great fun, but immensely effective:
+                    I can still remember the things I learned back then, even though it’s been
+                    almost two decades.<a href="#" class="link"></a></p>
+                <p>That is how I approach cooking as well. I am not only interested in the end
+                    product, but in learning the underlying principles. And there is no better
+                    way to learn than by having hands-on fun in a highly creative makerspace,
+                    with all the tools you could ever dream of, and a community to lend a
+                    hand.<a href="#" class="link"></a></p>
             </div>
         </div>
     </b-container>
