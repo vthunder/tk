5689858b5a3d5bd8ea216585327546391842088f
Dan Mills <hello@sandmill.org>
2019-01-20 14:13:31 -0800

    fix stripe key env var name

--- plugins/stripe-checkout.js
+++ plugins/stripe-checkout.js
@@ -1,4 +1,4 @@
 import Vue from 'vue'; // eslint-disable-line import/no-extraneous-dependencies
 import VueStripeCheckout from 'vue-stripe-checkout';
 
-Vue.use(VueStripeCheckout, process.env.VUE_APP_STRIPE_KEY);
+Vue.use(VueStripeCheckout, process.env.STRIPE_KEY);
