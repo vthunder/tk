fbdd06f59e233bc1db8ecf536640fd176834b0ca
Dan Mills <hello@sandmill.org>
2019-02-17 16:05:42 -0800

    add store to track checkin user data as user fills it in

--- store/checkin.js
+++ store/checkin.js
@@ -1,7 +1,13 @@
-import * as _ from 'lodash';
+import * as moment from 'moment';
 
 export const state = () => ({
   qrData: {},
+  userData: {
+    name: null,
+    email: null,
+    addToMailingList: null,
+    agreedToTerms: null,
+  },
 })
 
 export const mutations = {
@@ -10,5 +16,25 @@ export const mutations = {
   },
   clearQrData(state) {
     state.qrData = {};
+  },
+  setName(state, name) {
+    state.userData.name = name;
+  },
+  setEmail(state, email) {
+    state.userData.email = email;
+  },
+  setAddToMailingList(state, bool) {
+    state.userData.addToMailingList = bool;
+  },
+  setAgreedToTerms(state) {
+    state.userData.agreedToTerms = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+  },
+  clearUserData(state) {
+    state.userData = {
+      name: null,
+      email: null,
+      addToMailingList: null,
+      agreedToTerms: null,
+    };
   },
 }
