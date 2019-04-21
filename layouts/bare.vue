21b3c69b7547b785e9dda525f553f466eb678225
Dan Mills <hello@sandmill.org>
2019-04-20 20:07:06 -0700

    add redirect for blind tasting survey with bare layout

---
+++ layouts/bare.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <router-view id="router-view" />
+    <AuthModal/>
+    <WelcomeModal/>
+  </div>
+</template>
+
+<script>
+  import AuthModal from '@/components/AuthModal.vue';
+  import WelcomeModal from '@/components/WelcomeModal.vue';
+
+  export default {
+    components: {
+      AuthModal,
+      WelcomeModal,
+    },
+  };
+</script>
+
+<style lang="scss">
+  .async-hide { opacity: 0 !important}
+
+  #router-view {
+    min-height: 60vh;
+  }
+</style>
---
+++ pages/tasting.vue
@@ -0,0 +1,12 @@
+<template>
+  <b-container/>
+</template>
+
+<script>
+  export default {
+    layout: 'bare',
+    mounted() {
+      document.location = 'https://ybljsozu.paperform.co/';
+    },
+  };
+</script>
