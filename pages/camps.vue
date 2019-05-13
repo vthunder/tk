1ffaa5b6891805c205964bd2cbc6cf37a0e1cc0c
Dan Mills <hello@sandmill.org>
2019-05-12 18:03:03 -0700

    add camps redirect

---
+++ pages/camps.vue
@@ -0,0 +1,12 @@
+<template>
+  <b-container/>
+</template>
+
+<script>
+  export default {
+    layout: 'bare',
+    async mounted() {
+      document.location = 'https://culinaryartistas.getgalore.com/?locations%5B%5D=75518';
+    },
+  };
+</script>
