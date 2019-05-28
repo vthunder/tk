140384038812260a2985cfbd892ae1a9a6371c04
Dan Mills <hello@sandmill.org>
2019-05-27 22:35:47 -0700

    make signin link show up on mobile, remove hamburger when not needed

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,9 +1,12 @@
 <template>
   <b-navbar :class="{ subpage }" fixed="top" toggleable="md">
-    <b-navbar-toggle target="nav_collapse"><i class="fas fa-bars"/></b-navbar-toggle>
+    <b-navbar-toggle v-if="me.name" target="nav_collapse"><i class="fas fa-bars"/></b-navbar-toggle>
     <b-navbar-brand :to="{ name: 'index' }"/>
     <span/>
 
+    <b-nav-item v-b-modal.auth-modal v-if="!me.name">
+      <span class="signin-link">Sign in <i class="fas fa-sign-in-alt"/></span>
+    </b-nav-item>
     <b-collapse id="nav_collapse" is-nav>
       <b-navbar-nav class="ml-auto">
         <b-nav-item-dropdown v-if="me.name" :text="me.name" right>
@@ -14,9 +17,6 @@
           <b-dd-divider/>
           <b-dd-item-button @click="signout()">Sign out</b-dd-item-button>
         </b-nav-item-dropdown>
-        <b-nav-item v-b-modal.auth-modal v-else>
-          <span class="signin-link">Sign in <i class="fas fa-sign-in-alt"/></span>
-        </b-nav-item>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
