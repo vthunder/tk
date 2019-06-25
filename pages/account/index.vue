2872b6680ff1571ec9de155bea6462d2f73b625d
Dan Mills <hello@sandmill.org>
2019-06-24 23:06:53 -0700

    redirect /account -> /account/account

---
+++ pages/account/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <b-container/>
+</template>
+
+<script>
+  import * as misc from '@/graphql/misc';
+  export default {
+    layout: 'bare',
+    async mounted() {
+      document.location = '/account/account';
+    },
+  };
+</script>
