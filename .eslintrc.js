0bc20dc222cf2acc73f948fe6f9ad18dd8378325
Dan Mills <hello@sandmill.org>
2019-09-25 16:07:36 -0700

    Add eslint-plugin-json and reformat .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,19 +5,15 @@ module.exports = {
     node: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
-  extends: [
-    'plugin:vue/recommended'
-  ],
+  extends: ["plugin:vue/recommended"],
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  plugins: ["vue"],
   // add your custom rules here
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'vue/max-attributes-per-line': 'off',
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/max-attributes-per-line": "off"
   }
-}
+};
